324f8a9e52cfad69d97379f54d4c6670b5a25ba2
Madeline Zitella <mzitella@nd.edu>
2024-07-29 23:46:34 -0400

    script.js added, hamburger and nav menu finished

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links"); 
-    const menu = document.querySelector(".hamburger-icon"); 
-    menu.classlist.toggle("open"); 
-    icon.classlist.toggle("open"); 
-}
-
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
