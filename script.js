4af15da2421939d524acdda9d393d7bb131f57f9
Madeline Zitella <mzitella@nd.edu>
2024-07-29 23:35:40 -0400

    adding hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); 
+    const menu = document.querySelector(".hamburger-icon"); 
+    menu.classlist.toggle("open"); 
+    icon.classlist.toggle("open"); 
+}
+
